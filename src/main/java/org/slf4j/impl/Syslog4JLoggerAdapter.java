fb0e7b028a28f803f7b9234260e253acb6d61721
Jeffrey Sheehan <jeff.sheehan7@gmail.com>
2015-11-05 13:05:52 +1000

    Oops, bugs already

--- src/main/java/org/slf4j/impl/Syslog4JLoggerAdapter.java
+++ src/main/java/org/slf4j/impl/Syslog4JLoggerAdapter.java
@@ -17,14 +17,13 @@ import java.io.StringWriter;
  */
 public class Syslog4JLoggerAdapter extends MarkerIgnoringBase implements LocationAwareLogger, Serializable {
 
+    private static final long serialVersionUID = 1L;
     private final transient SyslogIF syslog;
     private Integer level;
 
     Syslog4JLoggerAdapter(SyslogIF syslog, Integer level) {
         this.syslog = syslog;
         this.level = level;
-        //SyslogIF syslog = Syslog.getInstance("udp");
-        //syslog.info("This is another INFO level log entry.");
     }
 
     public boolean isTraceEnabled() {
@@ -32,52 +31,60 @@ public class Syslog4JLoggerAdapter extends MarkerIgnoringBase implements Locatio
     }
 
     public void trace(String msg) {
-        // not for now
+        if (isTraceEnabled()) debug(msg);
     }
 
     public void trace(String format, Object arg) {
-        // not for now
+        if (isTraceEnabled()) debug(format, arg);
     }
 
     public void trace(String format, Object arg1, Object arg2) {
-        // not for now
+        if (isTraceEnabled()) debug(format, arg1, arg2);
     }
 
     public void trace(String format, Object... arguments) {
-        // not for now
+        if (isTraceEnabled()) debug(format, arguments);
     }
 
     public void trace(String msg, Throwable t) {
-        // not for now
+        if (isTraceEnabled()) debug(msg, t);
     }
 
     public boolean isDebugEnabled() {
-        if (level <= Syslog.LEVEL_DEBUG) return true;
+        if (level >= Syslog.LEVEL_DEBUG) return true;
         else return false;
     }
 
     public void debug(String msg) {
-        syslog.debug(msg);
+        if (isDebugEnabled()) syslog.debug(msg);
     }
 
     public void debug(String format, Object arg) {
-        FormattingTuple ft = MessageFormatter.format(format, arg);
-        syslog.debug(ft.getMessage());
+        if (isDebugEnabled()) {
+            FormattingTuple ft = MessageFormatter.format(format, arg);
+            syslog.debug(ft.getMessage());
+        }
     }
 
     public void debug(String format, Object arg1, Object arg2) {
-        FormattingTuple ft = MessageFormatter.format(format, arg1, arg2);
-        syslog.debug(ft.getMessage());
+        if (isDebugEnabled()) {
+            FormattingTuple ft = MessageFormatter.format(format, arg1, arg2);
+            syslog.debug(ft.getMessage());
+        }
     }
 
     public void debug(String format, Object... arguments) {
-        FormattingTuple ft = MessageFormatter.arrayFormat(format, arguments);
-        syslog.debug(ft.getMessage());
+        if (isDebugEnabled()) {
+            FormattingTuple ft = MessageFormatter.arrayFormat(format, arguments);
+            syslog.debug(ft.getMessage());
+        }
     }
 
     public void debug(String msg, Throwable t) {
-        syslog.debug(msg);
-        for (String line : getStacktrace(t)) syslog.debug(line);
+        if (isDebugEnabled()) {
+            syslog.debug(msg);
+            for (String line : getStacktrace(t)) syslog.debug(line);
+        }
     }
 
     public boolean isInfoEnabled() {
@@ -86,84 +93,109 @@ public class Syslog4JLoggerAdapter extends MarkerIgnoringBase implements Locatio
     }
 
     public void info(String msg) {
-        syslog.info(msg);
+        if (isInfoEnabled()) syslog.info(msg);
     }
 
     public void info(String format, Object arg) {
-        FormattingTuple ft = MessageFormatter.format(format, arg);
-        syslog.info(ft.getMessage());
+        if (isInfoEnabled()) {
+            FormattingTuple ft = MessageFormatter.format(format, arg);
+            syslog.info(ft.getMessage());
+        }
     }
 
     public void info(String format, Object arg1, Object arg2) {
-        FormattingTuple ft = MessageFormatter.format(format, arg1, arg2);
-        syslog.info(ft.getMessage());
+        if (isInfoEnabled()) {
+            FormattingTuple ft = MessageFormatter.format(format, arg1, arg2);
+            syslog.info(ft.getMessage());
+        }
     }
 
     public void info(String format, Object... argArray) {
-        FormattingTuple ft = MessageFormatter.arrayFormat(format, argArray);
-        syslog.info(ft.getMessage());
+        if (isInfoEnabled()) {
+            FormattingTuple ft = MessageFormatter.arrayFormat(format, argArray);
+            syslog.info(ft.getMessage());
+        }
     }
 
     public void info(String msg, Throwable t) {
-        syslog.info(msg);
-        for (String line : getStacktrace(t)) syslog.info(line);
+        if (isInfoEnabled()) {
+            syslog.info(msg);
+            for (String line : getStacktrace(t)) syslog.info(line);
+        }
     }
 
     public boolean isWarnEnabled() {
-        if (level <= Syslog.LEVEL_WARN) return true;
+        if (level >= Syslog.LEVEL_WARN) return true;
         else return false;
     }
 
     public void warn(String msg) {
-        syslog.warn(msg);
+        if (isWarnEnabled()) syslog.warn(msg);
     }
 
     public void warn(String format, Object arg) {
-        FormattingTuple ft = MessageFormatter.format(format, arg);
-        syslog.warn(ft.getMessage());
+        if (isWarnEnabled()) {
+            FormattingTuple ft = MessageFormatter.format(format, arg);
+            syslog.warn(ft.getMessage());
+        }
     }
 
     public void warn(String format, Object arg1, Object arg2) {
-        FormattingTuple ft = MessageFormatter.format(format, arg1, arg2);
-        syslog.warn(ft.getMessage());
+        if (isWarnEnabled()) {
+            FormattingTuple ft = MessageFormatter.format(format, arg1, arg2);
+            syslog.warn(ft.getMessage());
+        }
     }
 
     public void warn(String format, Object... argArray) {
-        FormattingTuple ft = MessageFormatter.arrayFormat(format, argArray);
-        syslog.warn(ft.getMessage());
+        if (isWarnEnabled()) {
+            FormattingTuple ft = MessageFormatter.arrayFormat(format, argArray);
+            syslog.warn(ft.getMessage());
+        }
     }
 
     public void warn(String msg, Throwable t) {
-        syslog.warn(msg);
-        for (String line : getStacktrace(t)) syslog.warn(line);
+        if (isWarnEnabled()) {
+            syslog.warn(msg);
+            for (String line : getStacktrace(t)) syslog.warn(line);
+        }
     }
 
     public boolean isErrorEnabled() {
-        if (level <= Syslog.LEVEL_ERROR) return true;
+        if (level >= Syslog.LEVEL_ERROR) return true;
         else return false;
     }
 
     public void error(String msg) {
-        syslog.error(msg);
+        if (isErrorEnabled()) syslog.error(msg);
     }
 
     public void error(String format, Object arg) {
-        FormattingTuple ft = MessageFormatter.format(format, arg);
-        syslog.error(ft.getMessage());
+        if (isErrorEnabled()) {
+            FormattingTuple ft = MessageFormatter.format(format, arg);
+            syslog.error(ft.getMessage());
+        }
     }
 
     public void error(String format, Object arg1, Object arg2) {
-        FormattingTuple ft = MessageFormatter.format(format, arg1, arg2);
-        syslog.error(ft.getMessage());
+        if (isErrorEnabled()) {
+            FormattingTuple ft = MessageFormatter.format(format, arg1, arg2);
+            syslog.error(ft.getMessage());
+        }
     }
 
     public void error(String format, Object... argArray) {
-        FormattingTuple ft = MessageFormatter.arrayFormat(format, argArray);
-        syslog.error(ft.getMessage());
+        if (isErrorEnabled()) {
+            FormattingTuple ft = MessageFormatter.arrayFormat(format, argArray);
+            syslog.error(ft.getMessage());
+        }
     }
 
     public void error(String msg, Throwable t) {
-        syslog.error(msg);
+        if (isErrorEnabled()) {
+            syslog.error(msg);
+            for (String line : getStacktrace(t)) syslog.error(line);
+        }
     }
 
     public void log(Marker marker, String callerFQCN, int level, String msg, Object[] argArray, Throwable t) {
--- src/test/java/TestSyslog4JLoggerAdapter.java
+++ src/test/java/TestSyslog4JLoggerAdapter.java
@@ -1,6 +1,10 @@
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 
 import java.io.IOException;
 
@@ -11,12 +15,18 @@ public class TestSyslog4JLoggerAdapter {
 
     private Logger logger = LoggerFactory.getLogger(TestSyslog4JLoggerAdapter.class);
 
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("syslog4j.ident", "test");
+        System.setProperty("syslog4j.facility", "auth");
+        System.setProperty("syslog4j.level", "INFO");
+    }
     @Test
     public void testLogging() {
         logger.debug("TEST debug");
         logger.info("TEST info!");
 
         logger.warn("Test throwable", new IOException("Some IOException"));
-
+        logger.error("Error message");
     }
 }
